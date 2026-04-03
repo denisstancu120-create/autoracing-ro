b57ec6e192717b698b7daecbef9efb5078f17451
denisstancu120-create <denisstancu120@gmail.com>
2026-04-02 20:05:13 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,40 +1,7 @@
-let allEvents = [];
-
-async function loadEvents() {
-  try {
-    const response = await fetch("./events.json");
-    const events = await response.json();
-
-    allEvents = events;
-
-    renderEvents(events);
-  } catch (error) {
-    console.error("Eroare:", error);
-  }
-}
-
-function renderEvents(events) {
-  const container = document.getElementById("eventsContainer");
-  if (!container) return;
-
-  container.innerHTML = "";
-
-  events.forEach(event => {
-    const card = document.createElement("div");
-    card.className = "event-card";
-
-    card.innerHTML = `
-      <img src="${event.image}">
-      <div class="event-content">
-        <h3>${event.title}</h3>
-        <p>${event.description}</p>
-      </div>
-    `;
-
-    container.appendChild(card);
-  });
-}
-
 document.addEventListener("DOMContentLoaded", () => {
-  loadEvents();
+  alert("JS FUNCTIONEAZA");
+
+  const container = document.getElementById("eventsContainer");
+
+  container.innerHTML = "<h2>TEST FUNCTIONEAZA</h2>";
 });
