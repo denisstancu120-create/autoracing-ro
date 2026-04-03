4be5f643cf5c9063df0e2b285a7ec66a961d8ab7
denisstancu120-create <denisstancu120@gmail.com>
2026-04-02 20:03:26 -0500

    Implement event loading and rendering
    Added event loading and rendering functionality.

--- script.js
+++ script.js
@@ -1,5 +1,40 @@
-console.log("SITE FUNCTIONEAZA");
+let allEvents = [];
+
+async function loadEvents() {
+  try {
+    const response = await fetch("./events.json");
+    const events = await response.json();
+
+    allEvents = events;
+
+    renderEvents(events);
+  } catch (error) {
+    console.error("Eroare:", error);
+  }
+}
+
+function renderEvents(events) {
+  const container = document.getElementById("eventsContainer");
+  if (!container) return;
+
+  container.innerHTML = "";
+
+  events.forEach(event => {
+    const card = document.createElement("div");
+    card.className = "event-card";
+
+    card.innerHTML = `
+      <img src="${event.image}">
+      <div class="event-content">
+        <h3>${event.title}</h3>
+        <p>${event.description}</p>
+      </div>
+    `;
+
+    container.appendChild(card);
+  });
+}
 
 document.addEventListener("DOMContentLoaded", () => {
-  document.body.style.background = "black";
+  loadEvents();
 });
